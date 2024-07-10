2b2a2253ae674f113c212b63a112fd47bcb0f354
Uri Kadushin <uri.kadushin@gmail.com>
2024-07-10 13:46:47 +0300

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace DisplayAMap
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            Esri.ArcGISRuntime.ArcGISRuntimeEnvironment.ApiKey = "AAPTxy8BH1VEsoebNVZXo8HurIaDWYvvUyjPrVsCC9dvPfJZ0Xkkt8eLXgb5ypkvm5Uq6wbaoVW - tX_zxPooZcOcvu0Gvi8iq3ii6uvJYYaW0ez8JTBtBTKQySc_FdV9ISrNENPbYk76sII364NTsU66AmNA - mkjSlhU0loOCvJImkQaiz5KQXXnSZcZWXQxO1 - I - RBSfs5IkOdRX86DWqhhsMKo_kpw2YvPKoy8ua39 - Vs.AT1_rxWg8l2y";
+        }
+    }
+
+
+}
---
+++ MapViewModel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Esri.ArcGISRuntime.Mapping;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Esri.ArcGISRuntime.Portal;
+
+namespace DisplayAMap
+{
+    internal class MapViewModel : INotifyPropertyChanged
+    {
+        public MapViewModel()
+        {
+            SetupMap();
+        }
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        private Map? _map;
+        public Map? Map
+        {
+            get { return _map; }
+            set
+            {
+                _map = value;
+                OnPropertyChanged();
+            }
+        }
+        private async Task SetupMap()
+        {
+
+            //// Create a new map with a 'topographic vector' basemap.
+            //Map = new Map(BasemapStyle.ArcGISTopographic);
+
+            // Create a portal. If a URI is not specified, www.arcgis.com is used by default.
+            ArcGISPortal portal = await ArcGISPortal.CreateAsync();
+
+            // Get the portal item for a web map using its unique item id.
+            PortalItem mapItem = await PortalItem.CreateAsync(portal, "d6896046b9b34086a47da90c53d11882");
+
+            // Create the map from the item.
+            Map map = new Map(mapItem);
+
+            // To display the map, set the MapViewModel.Map property, which is bound to the map view.
+            this.Map = map;
+
+        }
+    }
+}
